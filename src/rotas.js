6004dddf33f96ef02e23b2f240c35e80b0d3f9cd
Joao Chudek <e-joaochudek@weg.net>
2024-11-18 17:04:59 -0300

    fixed server and rotas to take views

--- src/rotas.js
+++ src/rotas.js
@@ -16,8 +16,8 @@ const handleRoute = (req, res) => {
            feedbackController.show(req, res, id);
        } else if (req.method === 'POST' && req.url.includes("/cadastrar")) {
            feedbackController.store(req, res);
-       } else if (req.method === 'PUT' && id && req.url.includes("/atualizar")) {
-        feedbackController.update(req, res);
+       } else if (req.method === 'PUT' && req.url.includes("/atualizar")) {
+           feedbackController.update(req, res);
        } else {
            res.writeHead(404, { 'Content-Type': 'text/plain' });
            res.end('Rota não encontrada');
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 const handleRoute = require('./rotas');
 const authMiddleware = require('./middleware/auth');
 
@@ -6,7 +8,40 @@ require('./database/database');
 
 const server = http.createServer((req, res) => {
    const next = () => handleRoute(req, res);
-   authMiddleware(req, res, next);
+
+   if (req.url === '/' && req.method === 'GET') {
+       fs.readFile(path.join(__dirname, 'view', 'formulario_view.html'), (err, content) => {
+           if (err) {
+               res.writeHead(500);
+               res.end('Erro ao carregar a página.');
+           } else {
+               res.writeHead(200, { 'Content-Type': 'text/html' });
+               res.end(content);
+           }
+       });
+   } else if (req.url === '/feedbacks' && req.method === 'GET') {
+       fs.readFile(path.join(__dirname, 'view', 'feedbacks_view.html'), (err, content) => {
+           if (err) {
+               res.writeHead(500);
+               res.end('Erro ao carregar a página.');
+           } else {
+               res.writeHead(200, { 'Content-Type': 'text/html' });
+               res.end(content);
+           }
+       });
+   } else if (req.url.startsWith('/feedbacks/') && req.method === 'GET') {
+       fs.readFile(path.join(__dirname, 'view', 'feedbacks_show_view.html'), (err, content) => {
+           if (err) {
+               res.writeHead(500);
+               res.end('Erro ao carregar a página.');
+           } else {
+               res.writeHead(200, { 'Content-Type': 'text/html' });
+               res.end(content);
+           }
+       });
+   } else {
+       authMiddleware(req, res, next);
+   }
 });
 
 server.listen(3000, () => {
